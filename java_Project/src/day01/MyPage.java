096ed109479291c1a5f691f3c72a6816f0de3ad3
jihye <lullu303@naver.com>
2022-07-22 20:39:23 +0900

    teamjava

---
+++ java_Project/src/day01/MyPage.java
@@ -0,0 +1,13 @@
+package day01;
+
+public class MyPage {
+
+	public static void main(String[] args) {
+
+		
+		
+		
+		
+	}
+
+}
---
+++ java_Project/src/day01/UserInfo.java
@@ -0,0 +1,20 @@
+package day01;
+
+public class UserInfo {
+
+	public static void main(String[] args) {
+		Mypage2 my=new Mypage2();
+		
+		System.out.println("전체 회원의 정보입니다"); //전체회원 객체 생성, 정보 출력하기
+		
+		
+		
+		
+		
+		
+		
+	
+
+	}
+
+}
